4665c6c64fb0078742e62b659e097ff5cc7673d8
ssu.techture@gmail.com <ssu.techture@gmail.com>
2025-07-12 19:13:13 +0530

    Refactor user details to remove warnings

--- src/app/components/user-details/user-details.ts
+++ src/app/components/user-details/user-details.ts
@@ -1,31 +1,46 @@
-import { Component, computed, inject, OnInit } from '@angular/core';
+import {
+  Component,
+  computed,
+  inject,
+  OnInit,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ActivatedRoute,RouterModule } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 
 import { DataFetch } from '../../common/services/data-fetch';
 import type { User } from '../../common/models/user.model';
-import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
+  standalone: true,
   selector: 'app-user-details',
-  imports: [CommonModule,RouterModule, MatCardModule, MatButtonModule, MatIconModule,MatDividerModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+  ],
   templateUrl: './user-details.html',
   styleUrl: './user-details.scss',
 })
-export class UserDetails implements OnInit{
+export class UserDetails implements OnInit {
   private route = inject(ActivatedRoute);
+  private router = inject(Router);
   private dataService = inject(DataFetch);
 
   users = this.dataService.users;
-  ngOnInit() {
-  if (!this.users() || this.users().length === 0) {
-    this.dataService.fetchUsers();
-  }
-}
 
   index = computed(() => Number(this.route.snapshot.paramMap.get('id')));
-  user = computed<User>(() => this.users()?.[this.index()]);
+  user = computed<User | undefined>(() => this.users()?.[this.index()]);
+
+  ngOnInit() {
+    if (!this.users() || this.users().length === 0) {
+      this.dataService.fetchUsers();
+    }
+  }
 }
